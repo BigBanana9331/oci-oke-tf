a464f8917ac7e7db9a3edba9032613ac3fcf35d1
Huy Nguyen <phantom39z1@gmail.com>
2026-01-19 15:04:17 +0700

    feat: add required provider

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "1.14.3"
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "7.30.0"
+    }
+  }
+}
+
 provider "oci" {
   region = "	ap-singapore-1"
 }
